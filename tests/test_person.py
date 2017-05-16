fb8f77553d75cdcf2c50423cb2e4159350a13e5a
peter paints <musonyengigi@gmail.com>
2017-05-16 11:05:27 +0300

    Fix test case for class Person

--- tests/test_person.py
+++ tests/test_person.py
@@ -5,5 +5,5 @@ from classes.person import Person
 class PersonClassTest(unittest.TestCase):
     def test_full_name_only_returns_strings(self):
         with self.assertRaises(ValueError, msg='Only strings are allowed as names'):
-            my_class_instance = Person("staff", "Peter", "Musonye")
+            my_class_instance = Person("staff", "Peter", 1234)
             my_class_instance.full_name()
