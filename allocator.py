e4a0686b6869ecb543af7db4b81c60d4233918a3
peter paints <musonyengigi@gmail.com>
2017-05-16 23:31:34 +0300

    Add smooth exit functionality even with Ctrl-C

--- allocator.py
+++ allocator.py
@@ -4,6 +4,7 @@ Office Space Allocator.
 Usage:
   allocator create_room <room_type> <room_name>...
   allocator add_person <person_name> <person_surname> <person_type> [<wants_accommodation>]
+  allocator exit
 
 Options:
   -h --help                         Show this screen.
@@ -15,6 +16,7 @@ Examples:
 
 """
 
+import sys
 import cmd
 from docopt import docopt, DocoptExit
 from classes.dojo import Dojo
@@ -28,18 +30,20 @@ def deco(f):
     of the docopt parsing to the called action.
     """
     def fn(self, arg):
+        """
+        The DocoptExit is thrown when the args do not match.
+        We print a message to the user and the usage block.
+        The SystemExit exception prints the usage for --help
+        """
         try:
             opt = docopt(fn.__doc__, arg)
 
         except DocoptExit as e:
-            # The DocoptExit is thrown when the args do not match.
-            # We print a message to the user and the usage block.
             print('Invalid Command!')
             print(e)
             return
 
         except SystemExit:
-            # The SystemExit exception prints the usage for --help
             return
 
         return f(self, opt)
@@ -51,39 +55,45 @@ def deco(f):
 
 
 class MyInteractive(cmd.Cmd):
-    intro = 'Welcome to the Office Space Allocator!' \
-        + ' (Type help for a list of commands.)'
+    intro = ('* ' * 10) + 'WELCOME TO THE OFFICE SPACE ALLOCATOR!' + (' *' * 10)\
+        + '\n' + ('= ' * 7) + 'the easiest way to handle all your room allocation.' + (' =' * 7) \
+        + '\n' + (' ' * 23) + 'Type help for a list of commands'
     prompt = '(allocator) '
 
     @deco
     def do_create_room(self, arg):
         """Usage: create_room <room_type> <room_name>..."""
         arguments = arg
-        # print (arguments)
-        if arguments['<room_name>'] and arguments['<room_type>']:
-            for room_name in arguments['<room_name>']:
-                dojo.create_room(arguments['<room_type>'], room_name)
+        try:
+            if arguments['<room_name>'] and arguments['<room_type>']:
+                for room_name in arguments['<room_name>']:
+                    dojo.create_room(arguments['<room_type>'], room_name)
+                    dojo.allocate_rooms()
+        except Exception as e:
+            print (e)
 
     @deco
     def do_add_person(self, arg):
         """Usage: add_person <person_name> <person_surname> <person_type> [<wants_accommodation>]"""
         arguments = arg
-        # print (arguments)
-        if arguments['<person_name>']:
-            if arguments['<person_type>'] and arguments['<person_type>'].lower() == 'staff':
-                dojo.add_person(arguments['<person_type>'], arguments['<person_name>'], arguments['<person_surname>'])
-                new_staff = dojo.all_persons[-1]
-                dojo.allocate_rooms()
-            elif arguments['<person_type>'] and arguments['<person_type>'].lower() == 'fellow':
-                if arguments['<wants_accommodation>']:
+        try:
+            if arguments['<person_name>']:
+                if arguments['<person_type>']:
                     dojo.add_person(arguments['<person_type>'], arguments['<person_name>'], arguments['<person_surname>'], arguments['<wants_accommodation>'])
                     new_fellow = dojo.all_persons[-1]
                     dojo.allocate_rooms()
-                else:
-                    dojo.add_person(arguments['<person_type>'], arguments['<person_name>'], arguments['<person_surname>'])
-                    new_fellow = dojo.all_persons[-1]
-                    dojo.allocate_rooms()
+        except Exception as e:
+            print (e)
 
+    @deco
+    def do_exit(self, arg):
+        """Usage: exit"""
+        print ('Have a nice day!')
+        exit()
 
 if __name__ == '__main__':
-    MyInteractive().cmdloop()
+    try:
+        MyInteractive().cmdloop()
+    except KeyboardInterrupt:
+        print ('Have a nice day!')
+        exit()
