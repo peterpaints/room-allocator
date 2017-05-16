653a40f7081457adaa6c6a1fd6cd166df4e5da83
peter paints <musonyengigi@gmail.com>
2017-05-17 02:24:56 +0300

    Fix tests hold objects in variables correctly

--- tests/test_room.py
+++ tests/test_room.py
@@ -9,21 +9,27 @@ class RoomClassTest(unittest.TestCase):
         self.my_dojo_instance = Dojo()
 
     def test_add_occupant(self):
-        new_fellow = self.my_dojo_instance.add_person("fellow", "Peter", "Musonye", "Y")
+        self.my_dojo_instance.add_person("fellow", "Peter", "Musonye", "Y")
+        new_fellow = self.my_dojo_instance.all_persons[-1]
         initial_persons_count = len(self.my_class_instance.persons)
         self.my_class_instance.add_occupant(new_fellow)
         new_persons_count = len(self.my_class_instance.persons)
         self.assertEqual(new_persons_count - initial_persons_count, 1)
 
     def test_cannot_add_more_than_max_occupants(self):
-        fellow_1 = self.my_dojo_instance.add_person("fellow", "Peter", "Musonye", "Y")
-        fellow_2 = self.my_dojo_instance.add_person("staff", "Farhan", "Abdi")
-        fellow_3 = self.my_dojo_instance.add_person("fellow", "Rose", "Maina", "Y")
-        fellow_4 = self.my_dojo_instance.add_person("fellow", "Dennis", "Kola", "Y")
-        fellow_5 = self.my_dojo_instance.add_person("fellow", "Eddy", "Karanja", "Y")
+        self.my_dojo_instance.add_person("fellow", "Peter", "Musonye", "Y")
+        fellow_1 = self.my_dojo_instance.all_persons[-1]
+        self.my_dojo_instance.add_person("staff", "Farhan", "Abdi")
+        fellow_2 = self.my_dojo_instance.all_persons[-1]
+        self.my_dojo_instance.add_person("fellow", "Rose", "Maina", "Y")
+        fellow_3 = self.my_dojo_instance.all_persons[-1]
+        self.my_dojo_instance.add_person("fellow", "Dennis", "Kola", "Y")
+        fellow_4 = self.my_dojo_instance.all_persons[-1]
+        self.my_dojo_instance.add_person("fellow", "Eddy", "Karanja", "Y")
+        fellow_5 = self.my_dojo_instance.all_persons[-1]
         self.my_class_instance.add_occupant(fellow_1)
         self.my_class_instance.add_occupant(fellow_2)
         self.my_class_instance.add_occupant(fellow_3)
         self.my_class_instance.add_occupant(fellow_4)
-        occupant_5 = self.my_class_instance.add_occupant(fellow_5)
-        self.assertEqual(occupant_5, "Room is at full capacity")
+        with self.assertRaises(Exception, msg="Office Blue is at full capacity"):
+            self.my_class_instance.add_occupant(fellow_5)
