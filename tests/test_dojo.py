2312b32647d38201663b360b7502f6cd413a52cc
peter paints <musonyengigi@gmail.com>
2017-05-18 15:26:35 +0300

    Remove redundant tests that checked if inputs to class dojo's methods are strings

--- tests/test_dojo.py
+++ tests/test_dojo.py
@@ -19,22 +19,12 @@ class DojoClassTest(unittest.TestCase):
         self.my_class_instance.create_room("office", "Blue")
         self.assertEqual(len(self.my_class_instance.all_rooms), 1, msg="The same room can't be created twice")
 
-    def test_inputs_create_room_are_strings(self):
-        """Test that room_type and room_name are strings."""
-        with self.assertRaises(ValueError, msg='Invalid: Please input a string as room_name or room_type'):
-            self.my_class_instance.create_room(1234, "Blue")
-
     def test_add_person_successfully(self):
         """Test that the list of all persons increases by one."""
         initial_person_count = len(self.my_class_instance.all_persons)
         self.my_class_instance.add_person("staff", "Neil", "Armstrong", "Y")
         new_person_count = len(self.my_class_instance.all_persons)
         self.assertEqual(new_person_count - initial_person_count, 1)
-
-    def test_inputs_add_person_are_strings(self):
-        """Test that person attributes are strings."""
-        with self.assertRaises(ValueError, msg='Only strings are allowed as input'):
-            self.my_class_instance.add_person("Fellow", "Peter", 23)
 
     def test_wants_accommodation_default_is_N(self):
         """Test that the option for wants_accommodation has a default of N."""
@@ -67,11 +57,6 @@ class DojoClassTest(unittest.TestCase):
         self.assertEqual(new_office.persons[-1], new_fellow)
         self.assertEqual(len(self.my_class_instance.allocated_persons), 1)
 
-    def test_room_name_to_be_printed_is_string(self):
-        """Test that user has passed in a string."""
-        with self.assertRaises(ValueError, msg="Please input room name as a string"):
-            self.my_class_instance.print_room(23)
-
     def test_that_rooms_must_exist_to_be_printed(self):
         """Test that error message is output of room does not exist."""
         with self.assertRaises(ValueError, msg="Black does not exist"):
@@ -93,7 +78,7 @@ class DojoClassTest(unittest.TestCase):
         new_office = self.my_class_instance.all_rooms[-1]
         self.my_class_instance.allocate_rooms()
         new_office_occupants = self.my_class_instance.print_room(new_office.room_name)
-        self.assertEqual(new_office_occupants, ("Peter Musonye Fellow\nPeter Muriuki Staff"))
+        self.assertListEqual(new_office_occupants, ["Peter Musonye Fellow", "Peter Muriuki Staff"])
 
     def test_it_outputs_to_a_txt_file_if_optioned(self):
         pass
