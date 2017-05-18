3bc75274adfd19b8c60b53303bf161554870f39d
peter paints <musonyengigi@gmail.com>
2017-05-19 00:54:40 +0300

    Shorten some line lengths to comply with PEP8 standards

--- classes/dojo.py
+++ classes/dojo.py
@@ -25,7 +25,8 @@ class Dojo(object):
         room_type = room_type.lower()
         room_name = room_name.lower()
         if room_type != "office" and room_type != "living_space":
-            raise ValueError("Invalid room type: Your room type should be 'office' or 'living_space'")
+            raise ValueError(
+                "Invalid room type: Your room type should be 'office' or 'living_space'")
         else:
             if room_type == 'office':
                 room = Office(room_name)
@@ -34,7 +35,8 @@ class Dojo(object):
             try:
                 for item in self.all_rooms:
                     if item.room_name == room_name and item.room_type == room_type:
-                        raise Exception(room_type.title() + " " + room_name.title() + " already exists")
+                        raise Exception(
+                            room_type.title() + " " + room_name.title() + " already exists")
             except Exception as e:
                 print (e)
             else:
@@ -43,31 +45,37 @@ class Dojo(object):
                 elif room.room_type == 'living_space':
                     self.living_spaces.append(room)
                 self.all_rooms.append(room)
-                print ("An " + room.room_type + " called " + room.room_name.title() + " has been successfully created!")
+                print ("An " + room.room_type + " called " +
+                       room.room_name.title() + " has been successfully created!")
 
     def add_person(self, person_type, person_name, person_surname, wants_accommodation="N"):
         if wants_accommodation:
             wants_accommodation = wants_accommodation.lower()
             if wants_accommodation != "y" and wants_accommodation != "n":
-                raise ValueError("Please input Y or N for wants_accommodation")
+                raise ValueError(
+                    "Please input Y or N for wants_accommodation")
         person_type = person_type.lower()
         person_name = person_name.lower()
         person_surname = person_surname.lower()
         if person_type != 'staff' and person_type != 'fellow':
-            raise ValueError("Invalid person_type: Your person type should be either 'fellow' or 'staff'")
+            raise ValueError(
+                "Invalid person_type: Your person type should be either 'fellow' or 'staff'")
         if person_type == 'staff':
             person = Staff(person_name, person_surname)
         elif person_type == 'fellow':
-            person = Fellow(person_name, person_surname, wants_accommodation)
+            person = Fellow(
+                person_name, person_surname, wants_accommodation)
         try:
             for item in self.all_persons:
                 if item.person_name == person_name and item.person_surname == person_surname:
-                    raise Exception(person_type.title() + " " + person_name.title() + " already exists")
+                    raise Exception(
+                        person_type.title() + " " + person_name.title() + " already exists")
         except Exception as e:
             print (e)
         else:
             self.all_persons.append(person)
-            print (person.person_type.title() + " " + person.person_name.title() + " " + person.person_surname.title() + " has been successfully added!")
+            print (person.person_type.title() + " " + person.person_name.title() +
+                   " " + person.person_surname.title() + " has been successfully added!")
 
     def allocate_rooms(self):
         if not self.all_persons:
@@ -77,50 +85,66 @@ class Dojo(object):
                 if person.person_type == "fellow" and person.wants_accommodation == "y":
                     if person not in self.those_allocated_living_spaces:
                         if self.living_spaces:
-                            random_living_space_number = randint(0, len(self.living_spaces) - 1)
+                            random_living_space_number = randint(
+                                0, len(self.living_spaces) - 1)
                             living_space_allocation = self.living_spaces[random_living_space_number]
                             try:
-                                living_space_allocation.add_occupant(person)
+                                living_space_allocation.add_occupant(
+                                    person)
                             except Exception as e:
                                 print (e)
-                            self.those_allocated_living_spaces.append(person)
+                            self.those_allocated_living_spaces.append(
+                                person)
                         else:
-                            print ("There are no living_spaces to allocate " + person.person_name.title() + " " + person.person_surname.title())
+                            print ("There are no living_spaces to allocate " +
+                                   person.person_name.title() + " " + person.person_surname.title())
                     if person not in self.those_allocated_offices:
                         if self.offices:
-                            random_office_number = randint(0, len(self.offices) - 1)
+                            random_office_number = randint(
+                                0, len(self.offices) - 1)
                             office_allocation = self.offices[random_office_number]
                             try:
-                                office_allocation.add_occupant(person)
+                                office_allocation.add_occupant(
+                                    person)
                             except Exception as e:
                                 print (e)
-                            self.those_allocated_offices.append(person)
+                            self.those_allocated_offices.append(
+                                person)
                         else:
-                            print ("There are no offices to allocate " + person.person_name.title() + " " + person.person_surname.title())
+                            print ("There are no offices to allocate " + person.person_name.title(
+                            ) + " " + person.person_surname.title())
                 elif person.person_type == "fellow" and person.wants_accommodation != "y":
                     if person not in self.those_allocated_offices:
                         if self.offices:
-                            random_office_number = randint(0, len(self.offices) - 1)
+                            random_office_number = randint(
+                                0, len(self.offices) - 1)
                             office_allocation = self.offices[random_office_number]
                             try:
-                                office_allocation.add_occupant(person)
+                                office_allocation.add_occupant(
+                                    person)
                             except Exception as e:
                                 print (e)
-                            self.those_allocated_offices.append(person)
+                            self.those_allocated_offices.append(
+                                person)
                         else:
-                            print ("There are no offices to allocate " + person.person_name.title() + " " + person.person_surname.title())
+                            print ("There are no offices to allocate " + person.person_name.title(
+                            ) + " " + person.person_surname.title())
                 elif person.person_type == "staff":
                     if person not in self.those_allocated_offices:
                         if self.offices:
-                            random_office_number = randint(0, len(self.offices) - 1)
+                            random_office_number = randint(
+                                0, len(self.offices) - 1)
                             office_allocation = self.offices[random_office_number]
                             try:
-                                office_allocation.add_occupant(person)
+                                office_allocation.add_occupant(
+                                    person)
                             except Exception as e:
                                 print (e)
-                            self.those_allocated_offices.append(person)
+                            self.those_allocated_offices.append(
+                                person)
                         else:
-                            print ("There are no offices to allocate " + person.person_name.title() + " " + person.person_surname.title())
+                            print ("There are no offices to allocate " + person.person_name.title(
+                            ) + " " + person.person_surname.title())
 
     def print_room(self, room_name):
         room_name = room_name.lower()
@@ -129,18 +153,20 @@ class Dojo(object):
             room_occupants = []
             if room.room_name == room_name:
                 found = True
-                print (room.room_type.title() + " " + room_name.title() + ":")
+                print (room.room_type.title() +
+                       " " + room_name.title() + ":")
                 if not room.persons:
-                    raise Exception(room.room_type.title() + " " + room.room_name.title() + " " + "has no occupants")
+                    raise Exception(room.room_type.title(
+                    ) + " " + room.room_name.title() + " " + "has no occupants")
                 else:
                     for occupant in room.persons:
-                        room_occupants.append(occupant.person_name.title() + " " + occupant.person_surname.title() + " " + occupant.person_type.title())
+                        room_occupants.append(occupant.person_name.title(
+                        ) + " " + occupant.person_surname.title() + " " + occupant.person_type.title())
             for occupant in room_occupants:
                 print (occupant)
         if not found:
             raise Exception(room_name.title() + " does not exist")
         return room_occupants
-
 
     def print_allocations(self, filename=None):
         if filename:
@@ -149,23 +175,27 @@ class Dojo(object):
             room_occupants = []
             try:
                 if not room.persons:
-                    raise Exception(room.room_type.title() + " " + room.room_name.title() + " " + "has no occupants")
+                    raise Exception(room.room_type.title(
+                    ) + " " + room.room_name.title() + " " + "has no occupants")
             except Exception:
                 pass
             else:
                 for occupant in room.persons:
-                    room_occupants.append(occupant.person_name.title() + " " + occupant.person_surname.title() + " " + occupant.person_type.title())
+                    room_occupants.append(occupant.person_name.title(
+                    ) + " " + occupant.person_surname.title() + " " + occupant.person_type.title())
             if filename:
                 filename = filename.lower()
                 f = open(filename + ".txt", "a")
                 f.write(" " + "\n")
-                f.write(room.room_type.title() + " " + room.room_name.title() + ":" + "\n")
+                f.write(room.room_type.title() + " " +
+                        room.room_name.title() + ":" + "\n")
                 f.write("-" * 23 + "\n")
                 for occupant in room_occupants:
                     f.write(occupant + "\n")
             else:
                 print (" ")
-                print (room.room_type.title() + " " + room.room_name.title() + ":")
+                print (room.room_type.title() + " " +
+                       room.room_name.title() + ":")
                 print ("-" * 23)
                 for occupant in room_occupants:
                     print (occupant)
@@ -179,9 +209,11 @@ class Dojo(object):
                 if filename:
                     filename = filename.lower()
                     f = open(filename + ".txt", "a")
-                    f.write(person.person_name.title() + " " + person.person_surname.title() + " " + person.person_type.title() + "\n")
+                    f.write(person.person_name.title(
+                    ) + " " + person.person_surname.title() + " " + person.person_type.title() + "\n")
                 else:
-                    print(person.person_name.title() + " " + person.person_surname.title() + " " + person.person_type.title())
+                    print(person.person_name.title(
+                    ) + " " + person.person_surname.title() + " " + person.person_type.title())
         if not found:
             if not self.all_persons:
                 print("There's no one in the system")
