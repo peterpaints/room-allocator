03bc00a09ba7aa3a0488eaf29ee49780c48b3102
peter paints <musonyengigi@gmail.com>
2017-05-31 01:16:35 +0300

    Introduce an id property to classes person, staff and fellow

--- classes/fellow.py
+++ classes/fellow.py
@@ -2,5 +2,5 @@ from classes.person import Person
 
 
 class Fellow(Person):
-    def __init__(self, person_name, person_surname, wants_accommodation="N", person_type="fellow"):
-        super(Fellow, self).__init__(person_type, person_name, person_surname, wants_accommodation)
+    def __init__(self, iden, person_name, person_surname, wants_accommodation="N", person_type="fellow"):
+        super(Fellow, self).__init__(iden, person_type, person_name, person_surname, wants_accommodation)
--- classes/person.py
+++ classes/person.py
@@ -1,9 +1,10 @@
 class Person(object):
-    def __init__(self, person_type, person_name, person_surname="", wants_accommodation="N"):
+    def __init__(self, iden, person_type, person_name, person_surname="", wants_accommodation="N"):
         self.person_name = person_name
         self.person_surname = person_surname
         self.person_type = person_type
         self.wants_accommodation = wants_accommodation
+        self.iden = iden
 
     def full_name(self):
         self.full_name = self.person_name + " " + self.person_surname
