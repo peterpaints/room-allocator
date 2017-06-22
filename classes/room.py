0136c9c25fc3009ca0707824c5f69e3fe9f2ef87
peterpaints <musonyengigi@gmail.com>
2017-06-22 12:56:04 +0300

    Add color to error messages

--- classes/room.py
+++ classes/room.py
@@ -1,3 +1,6 @@
+from termcolor import cprint
+
+
 class Room(object):
     def __init__(self, room_name, room_type, max_persons):
         self.room_name = room_name
@@ -6,10 +9,16 @@ class Room(object):
         self.persons = []
 
     def add_occupant(self, person):
+        """
+        Append person objects to self.persons.
+
+        However, len(self.persons) must be below the value of max_persons and
+        the person object being appended must not exist in self.persons
+        """
         if person not in self.persons:
             if len(self.persons) < self.max_persons:
                 self.persons.append(person)
-                print (person.person_type.title() + " " + person.person_name.title() + " " + person.person_surname.title() + " has been allocated " + self.room_type + " " + self.room_name.title())
+                cprint("\n" + person.person_type.title() + " " + person.person_name.title() + " " + person.person_surname.title() + " has been allocated " + self.room_type + " " + self.room_name.title(), 'cyan', attrs=['dark'])
             else:
                 raise Exception(self.room_type.title() + " " + self.room_name.title() + " is at full capacity")
         else:
