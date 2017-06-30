928ee4788740e3377c26013fc763e305c0b884d9
peterpaints <musonyengigi@gmail.com>
2017-06-30 15:40:02 +0300

    Add __repr__ method from base class to allow instantiation, which should be prevented in the base class only

--- classes/living_space.py
+++ classes/living_space.py
@@ -1,5 +1,9 @@
 from classes.room import Room
 
+
 class LivingSpace(Room):
     def __init__(self, room_name):
         super(LivingSpace, self).__init__(room_name, room_type="living_space", max_persons=4)
+
+    def __repr__(self):
+        pass
