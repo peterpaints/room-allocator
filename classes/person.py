292dc137603ae0bbcb5f73a8789d1b634d6ecf78
peterpaints <musonyengigi@gmail.com>
2017-06-30 15:37:39 +0300

    Change class into an ABC to prevent unauthorized instantiation

--- classes/person.py
+++ classes/person.py
@@ -1,7 +1,15 @@
-class Person(object):
+from abc import ABCMeta, abstractmethod
+
+
+class Person(metaclass=ABCMeta):
+
     def __init__(self, iden, person_type, person_name, person_surname="", wants_accommodation="N"):
         self.person_name = person_name
         self.person_surname = person_surname
         self.person_type = person_type
         self.wants_accommodation = wants_accommodation
         self.iden = iden
+
+    @abstractmethod
+    def __repr__(self):
+        pass
