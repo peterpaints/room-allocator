064d304f084b4bf05ab4dd13ff329668b4b216d1
peterpaints <musonyengigi@gmail.com>
2017-06-30 15:39:10 +0300

    Add __repr__ method from base class to allow instantiation, which should be prevented in the base class only

--- classes/fellow.py
+++ classes/fellow.py
@@ -4,3 +4,6 @@ from classes.person import Person
 class Fellow(Person):
     def __init__(self, iden, person_name, person_surname, wants_accommodation="N", person_type="fellow"):
         super(Fellow, self).__init__(iden, person_type, person_name, person_surname, wants_accommodation)
+
+    def __repr__(self):
+        pass
